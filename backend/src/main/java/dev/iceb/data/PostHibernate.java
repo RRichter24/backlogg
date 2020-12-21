c537d4b63f58da63bab5347708750234a70c5348
unknown <projectrobertson@gmail.com>
2020-12-20 22:14:17 -0600

    added backend functionality for creating new posts (controller to be implemented later)

---
+++ backend/src/main/java/dev/iceb/data/PostHibernate.java
@@ -0,0 +1,60 @@
+package dev.iceb.data;
+
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import dev.iceb.utils.HibernateUtil;
+
+import dev.iceb.beans.Post;
+
+public class PostHibernate implements PostDAO {
+	private HibernateUtil hu = HibernateUtil.getHibernateUtil();
+
+	public Post add(Post t) {
+		Session s = hu.getSession();
+		Transaction tx = null;
+		try {
+			tx = s.beginTransaction();
+			s.save(t);
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+		} finally {
+			s.close();
+		}
+		return t;
+	}
+
+	public Post getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set<Post> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void update(Post t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void delete(Post t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Post getByUserId(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Post getByUsername(String u) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
