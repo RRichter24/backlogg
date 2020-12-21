3501346617b1011eac20302360cd1f48eef97316
Ian Talla <ianroi_talla@yahoo.com>
2020-12-21 14:33:28 -0500

    Added routes to the frontend

--- client/client/src/app/app-routing.module.ts
+++ client/client/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeScreenComponent
+  },
+  {
+    path: "home",
+    component: HomeScreenComponent
+  }/*,
+  {
+    path: "user"
+  },
+  {},
+  {},
+  {}
+  */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
