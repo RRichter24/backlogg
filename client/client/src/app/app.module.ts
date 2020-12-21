a404f94882f58aa8a6414f968520853b852b746a
Christopher Cross <cc28@alumni.princeton.edu>
2020-12-21 13:07:57 -0800

    create services folder

--- client/client/src/app/app.module.ts
+++ client/client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule,
