a80ac4b5c5589f79274069a4bc2d4692e3bc4330
Christopher Cross <cc28@alumni.princeton.edu>
2020-12-23 11:10:29 -0800

    resolve conflicts

--- client/client/src/app/components/register-user/services/url.service.ts
+++ client/client/src/app/components/register-user/services/url.service.ts
@@ -7,11 +7,7 @@ export class UrlService {
 
   constructor() { }
 
-<<<<<<< HEAD:client/client/src/app/register-user/services/url.service.ts
-  getUrl(): String {
-=======
   public getUrl(): String {
->>>>>>> 057b3d728f039d2ee9a041e6a6474eef7409677b:client/client/src/app/components/register-user/services/url.service.ts
     return "jdbc:postgresql://usf2011.cjzzundlfqwz.us-east-2.rds.amazonaws.com:5432/postgres?currentSchema=backlogg/users";
   }
 }
