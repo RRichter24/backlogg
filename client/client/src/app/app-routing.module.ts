74cc90dd9f5ca576dda775d492b4ed3a5045b442
Ian Talla <ianroi_talla@yahoo.com>
2020-12-24 15:37:31 -0500

    Could not really add in registration stuff part 2

--- client/client/src/app/app-routing.module.ts
+++ client/client/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
-import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: AuthComponent},
-  {path: 'register', component: RegisterUserComponent},
+  {path: 'register', component: SignUpComponent},
 ];
 
 @NgModule({
