2fd857e565c9a2ee546977ea68b68603e09497cf
unknown <projectrobertson@gmail.com>
2020-12-23 22:05:06 -0600

    Added post.service to frontend and implemented addpost request method to new-post component

--- client/client/src/app/components/new-post/new-post.component.ts
+++ client/client/src/app/components/new-post/new-post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Person from 'src/app/models/person';
+import { PersonService } from 'src/app/services/person.service';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-new-post',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent implements OnInit {
+  personService: PersonService;
+  postService: PostService;
+  loggedUser: Person;
+  text: String;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loggedUser = this.personService.getLoggedUser();
+    this.text = '';
+  }
+
+  submitPost() {
+    this.postService.submitNewPost(this.text, this.loggedUser.id);
   }
 
 }
---
+++ client/client/src/app/services/post.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UrlService } from './url.service';
+import  Post  from '../models/post';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
+import { NewPostComponent } from '../components/new-post/new-post.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private postsUrl: string;
+
+  constructor(private http: HttpClient, private urlService: UrlService) { 
+    this.postsUrl = this.urlService.getUrl() + 'posts';
+  }
+  submitNewPost(posttext: String, posterId: Number): Observable<Post> {
+    return this.http.post(this.postsUrl, {withCredentials: true}).pipe();
+  }
+}
