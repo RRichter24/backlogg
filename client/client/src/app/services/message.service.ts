1da0ece602d3fe1d52a8254e498630449af6158f
Robert Richter <richterjrobert@gmail.com>
2020-12-27 11:34:00 -0500

    Creating message service for client side

---
+++ client/client/src/app/services/message.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import Message from '../models/message';
+import { UrlService } from './url.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  private messageUrl: string;
+
+  constructor(private http: HttpClient, private urlService: UrlService) {
+    this.messageUrl = this.urlService.getUrl() + 'message';
+   }
+
+   createNewMessage(message_text: string, sender_id: number, receiver_id: number): Observable<Message> {
+    let newMessage : Message = new Message();
+    newMessage.message_text = message_text;
+    newMessage.sender_id = sender_id;
+    newMessage.receiver_id = receiver_id;
+
+    return this.http.post(this.messageUrl, newMessage, {withCredentials: true}).pipe(
+      map(resp => resp as Message)
+    );
+   }
+}
