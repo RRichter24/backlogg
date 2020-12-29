6efee105fce3bf4107c5c93e991128f8fcb30741
Robert Richter <richterjrobert@gmail.com>
2020-12-28 21:43:26 -0500

    Added FriendRequest Code to back end

---
+++ backend/src/main/java/dev/iceb/data/RequestStatusDAO.java
@@ -0,0 +1,7 @@
+package dev.iceb.data;
+
+import dev.iceb.beans.RequestStatus;
+
+public interface RequestStatusDAO extends GenericDAO<RequestStatus> {
+
+}
---
+++ backend/src/main/java/dev/iceb/services/RequestStatusServiceImpl.java
@@ -0,0 +1,40 @@
+package dev.iceb.services;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import dev.iceb.beans.RequestStatus;
+import dev.iceb.data.RequestStatusDAO;
+
+
+@Service
+public class RequestStatusServiceImpl implements RequestStatusService {
+	
+	private RequestStatusDAO rsDao;
+	
+	@Override
+	public RequestStatus addRequestStatus(RequestStatus f) {
+		return rsDao.add(f);
+	}
+
+	@Override
+	public RequestStatus getRequestStatusById(Integer id) {
+		return rsDao.getById(id);
+	}
+
+	@Override
+	public Set<RequestStatus> getAllRequestStati() {
+		return rsDao.getAll();
+	}
+
+	@Override
+	public void updateRequestStatus(RequestStatus f) {
+		rsDao.update(f);
+	}
+
+	@Override
+	public void deleteRequestStatus(RequestStatus f) {
+		rsDao.delete(f);
+	}
+}
