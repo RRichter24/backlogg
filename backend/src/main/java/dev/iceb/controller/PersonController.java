90643a6121e0c3dd9f8f3ca8b4f3d398a4428631
Robert Richter <richterjrobert@gmail.com>
2020-12-29 23:06:03 -0500

    Added Mockito Tests for Person and Post and updated Pom for Mockito depends

--- backend/src/main/java/dev/iceb/controller/PersonController.java
+++ backend/src/main/java/dev/iceb/controller/PersonController.java
@@ -43,7 +43,6 @@ public class PersonController {
 	
 	@PutMapping
 	public ResponseEntity<Person> logIn(HttpSession session, @RequestParam("user") String username, @RequestParam("pass") String password){
-		System.out.println("hitting the login endpoint");
 		Person person = personServ.getByUsername(username);
 		if (person != null) {
 			if (person.getPasswd().equals(password)) {
@@ -57,7 +56,6 @@ public class PersonController {
 	
 	@PostMapping
 	public ResponseEntity<Person> registerPerson(HttpSession session, @RequestBody Person person) {
-		System.out.println("The Deadly Six: Zazz, Zomom, Zavik, Zor, etc, etc");
 		personServ.add(person);
 		return ResponseEntity.ok(person);
 	}
