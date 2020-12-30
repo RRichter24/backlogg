faa56be8ce85cf43070957dada5d75d3bd12d34d
Ian Talla <ianroi_talla@yahoo.com>
2020-12-30 10:15:43 -0500

    Merging preparation: Tried to add ability search for people

--- client/client/src/app/components/navbar/navbar.component.ts
+++ client/client/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonService } from 'src/app/services/person.service';
+
+import Person from 'src/app/models/person';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  wantedUsername: string;
+
+  constructor(private personService: PersonService) {
+    this.wantedUsername = '';
+  }
 
   ngOnInit(): void {
+  }
+
+  lookUpPerson(): void{
+    this.personService.getPersonByUsername(this.wantedUsername).subscribe(resp => {
+      console.log(resp);
+      let retrievedPerson: Person = resp;
+
+      alert("I have retrieved the person with the username " + retrievedPerson.username);
+    });
   }
 
 }
