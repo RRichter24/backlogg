7635bf9468610bc6830f3c9e81ce524fcdcbd3a7
Ian Talla <ianroi_talla@yahoo.com>
2020-12-31 10:41:36 -0500

    Failed attempts to test frontend

---
+++ client/client/src/app/components/auth/auth.component.mock.ts
@@ -0,0 +1,11 @@
+
+
+export class MockAuth {
+
+    username = 'glacia';
+    password = 'walrein';
+    isValidUsername = true;
+    isValidPassword = true;
+    btnStyle = 'btn-lg btn-primary btn-block btn-block';
+
+}
--- client/client/src/app/components/auth/auth.component.spec.ts
+++ client/client/src/app/components/auth/auth.component.spec.ts
@@ -2,15 +2,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthComponent } from './auth.component';
 
+import { PersonService } from '../../services/person.service';
+import { SessionStorageWrapperService } from '../../services/session-storage-wrapper.service';
+import { MockAuth } from './auth.component.mock';
+
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
+  let personService: PersonService;
+  let ssws: SessionStorageWrapperService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthComponent ]
+      declarations: [ AuthComponent ],
+      providers: [
+        { provide: PersonService, useClass: MockAuth },
+        { provide: SessionStorageWrapperService, useClass: MockAuth }
+      ]
     })
     .compileComponents();
+
+    component = TestBed.inject(AuthComponent);
+    personService = TestBed.inject(PersonService);
+    ssws = TestBed.inject(SessionStorageWrapperService);
+
   });
 
   beforeEach(() => {
@@ -22,4 +37,22 @@ describe('AuthComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('can determine whether you can log in', () => {
+    component.username = '';
+    component.password = '';
+    component.validateInput();
+    expect(component.btnStyle).toBe("btn btn-lg btn-primary btn-block disabled");
+
+    component.username = 'karma police, arrest this man';
+    component.password = 'he talks in maths. he buzzes like a fridge';
+    component.validateInput();
+    expect(component.btnStyle).toBe("btn btn-lg btn-primary btn-block");
+  });
+
+  it('can log a valid user in', () => {
+    component.onSignIn();
+    expect(ssws.getLoggedUser().passwd).toBe("walrein");
+  })
+
 });
