32148cbc27c3ca79e46e3ea18ef579afa9aa22ec
Ian Talla <ianroi_talla@yahoo.com>
2020-12-31 12:36:22 -0500

    Just to clear up what I have: Chris added crucial server part

--- client/client/src/app/components/auth/auth.component.spec.ts
+++ client/client/src/app/components/auth/auth.component.spec.ts
@@ -2,31 +2,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthComponent } from './auth.component';
 
-import { PersonService } from '../../services/person.service';
-import { SessionStorageWrapperService } from '../../services/session-storage-wrapper.service';
-import { Router } from '@angular/router';
-import { MockAuth } from './auth.component.mock';
+
+import Person from 'src/app/models/person';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
-  let personService: PersonService;
-  let ssws: SessionStorageWrapperService;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthComponent ],
-      providers: [
-        { provide: PersonService, useClass: MockAuth },
-        { provide: SessionStorageWrapperService, useClass: MockAuth },
-        { provide: Router, useClass: MockAuth}
-      ]
+      declarations: [ AuthComponent ]
     })
     .compileComponents();
-
-    component = TestBed.inject(AuthComponent);
-    personService = TestBed.inject(PersonService);
-    ssws = TestBed.inject(SessionStorageWrapperService);
 
   });
 
@@ -38,23 +26,7 @@ describe('AuthComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
+  });    
 
-  it('can determine whether you can log in', () => {
-    component.username = '';
-    component.password = '';
-    component.validateInput();
-    expect(component.btnStyle).toBe("btn btn-lg btn-primary btn-block disabled");
-
-    component.username = 'karma police, arrest this man';
-    component.password = 'he talks in maths. he buzzes like a fridge';
-    component.validateInput();
-    expect(component.btnStyle).toBe("btn btn-lg btn-primary btn-block");
-  });
-
-  it('can log a valid user in', () => {
-    component.onSignIn();
-    expect(ssws.getLoggedUser().passwd).toBe("walrein");
-  })
 
 });
